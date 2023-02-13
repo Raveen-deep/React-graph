6fb069bb5e526245532ca33d58ceaddacc810466
Raveen Deep <Raveendeep.singh@aidetic.in>
2023-02-13 22:37:01 +0530

    Add styling and bug fixes on graph

--- src/App.js
+++ src/App.js
@@ -8,165 +8,239 @@ import { Formik, Form, FieldArray } from "formik";
 import PieGraph from "./components/PieGraph";
 import { useState } from "react";
 import { Responsive, WidthProvider } from "react-grid-layout";
+import { TextField, Button } from "@mui/material";
+import DeleteOutlined from "@mui/icons-material/DeleteOutlined";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 function App() {
-  const ResponsiveReactGridLayout = WidthProvider(Responsive);
-  const [value, setValue] = useState("lineChart");
-  let label = [];
-  let data = [];
-  const initialValues = {
-    name: "",
-    graphValues: [
-      {
-        label: "",
-        value: 0,
-      },
-    ],
-  };
+    const ResponsiveReactGridLayout = WidthProvider(Responsive);
+    const [value, setValue] = useState("lineChart");
+    const [data, setData] = useState([]);
+    const [label, setLabel] = useState([]);
+    // let label = [];
+    // let data = [];
+    const initialValues = {
+        name: "",
+        graphValues: [
+            {
+                label: "",
+                value: 0,
+            },
+        ],
+    };
 
-  const addMore = (push) => {
-    push({
-      label: "",
-      value: 0,
-    });
-  };
+    const addMore = (push) => {
+        push({
+            label: "",
+            value: 0,
+        });
+    };
 
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    };
 
-  const onSubmit = (v) => {
-    console.log(v);
-    v.graphValues.map((ele) => {
-      label.push(ele.label);
-      data.push(parseInt(ele.value));
-      return ele;
-    });
-  };
+    const onSubmit = (v) => {
+        console.log(v);
+        let data=[]
+        let label=[]
+        v.graphValues.map((ele) => {
+            label.push(ele.value)
+            data.push(parseInt(ele.value));
+            return ele;
+        });
+        setLabel(label);
+        setData(data);
+    };
 
-  return (
-    <ResponsiveReactGridLayout className="layout" layouts={[]}>
-      <div
-        style={{ border: "solid 2px black" }}
-        key="a"
-        data-grid={{ x: 0, y: 0, w: 2, h: 2 }}
-      >
-        <div>Dashboard</div>
-        <select value={value} onChange={handleChange}>
-          <option value="lineChart">Line Chart</option>
-          <option value="barChart">Bar Chart</option>
-          <option value="pieChart">Pie Chart</option>
-        </select>
-      </div>
-      <div
-        style={{ border: "solid 2px black", overflow: "hidden" }}
-        key="b"
-        data-grid={{ x: 1, y: 0, w: 3, h: 3 }}
-      >
-        <Formik
-          initialValues={initialValues}
-          onSubmit={onSubmit}
-          enableReinitialize={true}
-        >
-          {(formik) => (
-            <Form>
-              <input
-                placeholder="Name"
-                type="text"
-                onChange={(e) => {
-                  formik.setFieldValue("name", e.target.value);
-                }}
-              ></input>
-
-              <FieldArray name={`graphValues`}>
-                {({ remove, push }) => (
-                  <>
-                    {formik.values.graphValues.length > 0 &&
-                      formik.values.graphValues.map((graphValues, index) => (
-                        <div key={index}>
-                          <input
-                            placeholder="Label"
-                            type="text"
-                            onChange={(e) => {
-                              formik.setFieldValue(
-                                `graphValues.${index}.label`,
-                                e.target.value
-                              );
-                            }}
-                          ></input>
-                          <input
-                            placeholder="Value"
-                            type="tel"
-                            onChange={(e) => {
-                              formik.setFieldValue(
-                                `graphValues.${index}.value`,
-                                e.target.value
-                              );
-                            }}
-                          ></input>
-                          {index > 0 ? (
-                            <button type="button" onClick={() => remove(index)}>
-                              Remove
-                            </button>
-                          ) : (
-                            <></>
-                          )}
-                        </div>
-                      ))}
-                    <br />
-                    <button
-                      type="button"
-                      className="textblue font-bold text-[14px]  w-[130px] mb-5"
-                      onClick={() => addMore(push)}
+    return (
+        <ResponsiveReactGridLayout className="layout" layouts={[]}>
+            <div
+                style={{ border: "solid 1px gray",backgroundColor:'#f1f8e9' }}
+                key="a"
+                data-grid={{ x: 0, y: 0, w: 3, h: 2 }}
+                className="px-5 py-5"
+            >
+                <FormControl size="small">
+                    <InputLabel >Select Chart</InputLabel>
+                    <Select
+                       style={{color:'#111414'}}
+                        label="Select Chart" onChange={handleChange} value={value} 
                     >
-                      + Add More
-                    </button>
-                    <button
-                      type="submit"
-                      className="textblue font-bold text-[14px]  w-[130px] mb-5"
-                    >
-                      Submit
-                    </button>
-                  </>
-                )}
-              </FieldArray>
-            </Form>
-          )}
-        </Formik>
-      </div>
-      <div
-        style={{ border: "solid 2px black" }}
-        key="c"
-        data-grid={{ x: 3, y: 0, w: 3, h: 2 }}
-      >
-        <LineGraph
-          data={{ x_axis_data: label, y_axis_data: data, label: value }}
-        />
-      </div>
-      <div
-        style={{ border: "solid 2px black" }}
-        key="d"
-        data-grid={{ x: 4, y: 0, w: 3, h: 2 }}
-      >
-        {data && label && data.length !== 0 && label.length !== 0 && (
-          <BarGraph
-            data={{ x_axis_data: label, y_axis_data: data, label: value }}
-          />
-        )}
-      </div>
-      <div
-        style={{ border: "solid 2px black" }}
-        key="e"
-        data-grid={{ x: 5, y: 0, w: 3, h: 2 }}
-      >
-        {data && label && data.length !== 0 && label.length !== 0 && (
-          <PieGraph
-            data={{ x_axis_data: label, y_axis_data: data, label: value }}
-          />
-        )}
-      </div>
-    </ResponsiveReactGridLayout>
-  );
+                        <MenuItem value="lineChart">Line Chart</MenuItem>
+                        <MenuItem value="barChart">Bar Chart</MenuItem>
+                        <MenuItem value="pieChart">Pie Chart</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+            <div
+                className="px-2 py-2"
+                style={{ border: "solid 1px gray", background: "#71639E" }}
+                key="b"
+                data-grid={{ x: 3, y: 0, w: 9, h: 2 }}
+            >
+                <Formik
+                    initialValues={initialValues}
+                    onSubmit={onSubmit}
+                    enableReinitialize={true}
+                >
+                    {(formik) => (
+                        <Form>
+                            {/* <TextField
+                                size="small"
+                                placeholder="Name"
+                                variant="outlined"
+                                style={{ backgroundColor: "white" }}
+                                onChange={(e) => {
+                                    formik.setFieldValue(
+                                        "name",
+                                        e.target.value
+                                    );
+                                }}
+                            /> */}
+
+                            <FieldArray name={`graphValues`}>
+                                {({ remove, push }) => (
+                                    <>
+                                        {formik.values.graphValues.length > 0 &&
+                                            formik.values.graphValues.map(
+                                                (graphValues, index) => (
+                                                    <div key={index}>
+                                                        <TextField
+                                                            className="mt-2"
+                                                            size="small"
+                                                            placeholder="Label"
+                                                            variant="outlined"
+                                                            style={{
+                                                                backgroundColor:
+                                                                    "white",
+                                                            }}
+                                                            onChange={(e) => {
+                                                                formik.setFieldValue(
+                                                                    `graphValues.${index}.label`,
+                                                                    e.target
+                                                                        .value
+                                                                );
+                                                            }}
+                                                        />
+                                                        <TextField
+                                                            className="mt-2 mx-2"
+                                                            size="small"
+                                                            placeholder="Value"
+                                                            type="number"
+                                                            variant="outlined"
+                                                            style={{
+                                                                backgroundColor:
+                                                                    "white",
+                                                            }}
+                                                            onChange={(e) => {
+                                                                formik.setFieldValue(
+                                                                    `graphValues.${index}.value`,
+                                                                    e.target
+                                                                        .value
+                                                                );
+                                                            }}
+                                                        />
+
+                                                        {index > 0 ? (
+                                                            <DeleteOutlined
+                                                                onClick={() =>
+                                                                    remove(
+                                                                        index
+                                                                    )
+                                                                }
+                                                                className="mt-1"
+                                                                fontSize="small"
+                                                                variant="outlined"
+                                                                style={{
+                                                                    color: "white",
+                                                                }}
+                                                            />
+                                                        ) : (
+                                                            <></>
+                                                        )}
+                                                    </div>
+                                                )
+                                            )}
+                                        <Button
+                                            variant="contained"
+                                            color="success"
+                                            size="small"
+                                            onClick={() => addMore(push)}
+                                        >
+                                            {" "}
+                                            + Add More
+                                        </Button>
+
+                                        <Button
+                                            variant="contained"
+                                            className="mx-2"
+                                            size="small"
+                                            color="success"
+                                            type="submit"
+                                        >
+                                            Submit
+                                        </Button>
+                                    </>
+                                )}
+                            </FieldArray>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
+            <div
+                style={{ border: "solid 1px gray", background: "#eeeeee" }}
+                key="c"
+                data-grid={{ x: 0, y: 6, w: 4, h: 2 }}
+            >
+                <LineGraph
+                    data={{
+                        x_axis_data: label,
+                        y_axis_data: data,
+                        label: 'Line Chart',
+                    }}
+                />
+            </div>
+            <div
+                style={{ border: "solid 1px gray", background: "#d2f7d2" }}
+                key="d"
+                data-grid={{ x: 4, y: 0, w: 4, h: 2 }}
+            >
+                {/* {data && label && data.length !== 0 && label.length !== 0 && ( */}
+                    <BarGraph
+                        data={{
+                            x_axis_data: label,
+                            y_axis_data: data,
+                            label: 'Bar Chart',
+                        }}
+                    />
+                {/* )} */}
+            </div>
+            <div
+                style={{ border: "solid 1px gray", background: "#643F0D" }}
+                key="e"
+                data-grid={{ x: 8, y: 0, w: 4, h: 2 }}
+            >
+                {/* {data && label && data.length !== 0 && label.length !== 0 && (
+                     <PieGraph
+                        data={{
+                            x_axis_data: label,
+                            y_axis_data: data,
+                            label: value,
+                        }}
+                    /> 
+                 )}  */}
+                <div className="text-center mt-5"  style={{color:'white'}}>
+                <h1>97</h1>
+                <h5>Tile (Layout5)</h5>
+                <p style={{fontSize:'14px'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                </div>
+            </div>
+        </ResponsiveReactGridLayout>
+    );
 }
 
 export default App;
